b62eb8ff41c9906209950e8f4d03d5f43674cf44
Diogo Reis <up201505472@fe.up.pt>
2017-11-04 19:02:17 +0000

    Checks if username and email are already in database, error appears in red and before updating the page to avoid reloading

---
+++ #justDoIt/account/CheckIfExists.php
@@ -0,0 +1,34 @@
+<?php
+    require '../database/connection.php';
+
+    $Email = $_POST['email'];
+    $Username = $_POST['username'];
+    if(!empty($Email) && !empty($Username))
+    {
+        $query = $conn->prepare('SELECT * FROM users WHERE username = :username');
+        $query->bindParam(':username', $Username);
+        $query->execute();
+        
+        $queryResults = $query->fetchAll();
+
+        if(count($queryResults) > 0)
+        {
+            echo -1;
+            return;
+        }
+
+        $query = $conn->prepare('SELECT * FROM users WHERE email = :email');
+        $query->bindParam(':email', $Email);
+        $query->execute();
+        
+        $queryResults = $query->fetchAll();  
+        
+        if(count($queryResults) > 0)
+        {
+            echo -2;
+            return;
+        }
+
+        echo 0; 
+    }
+?>
