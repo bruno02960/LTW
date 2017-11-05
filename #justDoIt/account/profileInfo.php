089af44450bdcd612a95b0eb64e780e823163662
bruno02960 <briguel1997>
2017-11-05 21:22:53 +0000

    Profile page stylized

--- #justDoIt/account/profileInfo.php
+++ #justDoIt/account/profileInfo.php
@@ -1,11 +1,22 @@
 <body>
 
-    <div class="profile">
-        <p> <?= $user['username']; ?> </p>
+    <section id="profile">
+      <h1> <?= $user['username']; ?> </h1>
+      <div id="pic">
+        PROFILE <br> PICTURE
+      </div>
+      <div id="info">
+        <h4> Email </h4>
         <p> <?= $user['email']; ?> </p>
+        <h4> Name </h4>
         <p> <?= $user['name']; ?> </p>
+        <h4> Register Date </h4>
         <p> <?= $user['registerDate']; ?> </p>
-        <a href = "profileEditor.php"> Edit Profile </a>
+      </div>
+      <br>
+      <div id="editProfileButton">
+      <a href = "profileEditor.php"> Edit Profile </a>
     </div>
+    </section>
 
 </body>
