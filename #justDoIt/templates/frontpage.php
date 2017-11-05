cdacd2ab40c4801152eac3e0d5c3bf67c053e8d6
bruno02960 <briguel1997>
2017-11-05 23:23:56 +0000

    Frontpage style

--- #justDoIt/templates/frontpage.php
+++ #justDoIt/templates/frontpage.php
@@ -1,36 +1,92 @@
-    <nav id="menu">
-      <ul>
-        <li><a href="index.html">Completed</a></li>
-        <li><a href="index.html">To Complete</a></li>
-        <li><a href="index.html">Expiring</a></li>
-      </ul>
-    </nav>
-    <section id="list">
       <?php if(isset($user)): ?>
+        <nav id="menu">
+          <ul>
+            <li><a href="index.html">Completed</a></li>
+            <li><a href="index.html">To Complete</a></li>
+            <li><a href="index.html">Expiring</a></li>
+          </ul>
+        </nav>
+          <section id="list">
+        <h1> Selected list name </h1>
+        <table class="tg">
+  <tr>
+    <th class="tg-yw4l">Status</th>
+    <th class="tg-yw4l">Task</th>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">incomplete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">complete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">incomplete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">complete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">incomplete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l">complete</td>
+    <td class="tg-yw4l">ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+  </tr>
+</table>
+</section>
+<aside id="lists">
+<ul>
+<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+<li>Aliquam tincidunt mauris eu risus.</li>
+<li>Vestibulum auctor dapibus neque.</li>
+<li>Nunc dignissim risus id metus.</li>
+<li>Cras ornare tristique elit.</li>
+<li>Vivamus vestibulum nulla nec ante.</li>
+<li>Praesent placerat risus quis eros.</li>
+<li>Fusce pellentesque suscipit nibh.</li>
+<li>Integer vitae libero ac risus egestas placerat.</li>
+<li>Vestibulum commodo felis quis tortor.</li>
+<li>Ut aliquam sollicitudin leo.</li>
+<li>Cras iaculis ultricies nulla.</li>
+<li>Donec quis dui at dolor tempor interdum.</li>
+<li>Vivamus molestie gravida turpis.</li>
+<li>Fusce lobortis lorem at ipsum semper sagittis.</li>
+<li>Nam convallis pellentesque nisl.</li>
+<li>Integer malesuada commodo nulla.</li>
+</ul>
+</aside>
       <?php else: ?>
+        <section id="welcome">
+            <p>Do it <br>
+  Just do it<br>
 
-        <h1> Please Login Or Register </h1>
+  Don't let your dreams be dreams<br>
+  Yesterday you said tomorrow<br>
+  So just do it<br>
+  Make your dreams come true<br>
+  Just do it<br>
 
+  Some people dream of success<br>
+  While you're gonna wake up and work hard at it<br>
+  Nothing is impossible<br>
+
+  You should get to the point<br>
+  Where anyone else would quit<br>
+  And you're not going to stop there<br>
+  No, what are you waiting for?<br>
+
+  Do it<br>
+  Just do it<br>
+  Yes you can<br>
+  Just do it<br>
+  If you're tired of starting over<br>
+  Stop giving up <br> <br>
+- Shia LaBeouf</p>
+</section>
       <?php endif; ?>
     </section>
-    <aside id="lists">
-      <ul>
-      <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-      <li>Aliquam tincidunt mauris eu risus.</li>
-      <li>Vestibulum auctor dapibus neque.</li>
-      <li>Nunc dignissim risus id metus.</li>
-      <li>Cras ornare tristique elit.</li>
-      <li>Vivamus vestibulum nulla nec ante.</li>
-      <li>Praesent placerat risus quis eros.</li>
-      <li>Fusce pellentesque suscipit nibh.</li>
-      <li>Integer vitae libero ac risus egestas placerat.</li>
-      <li>Vestibulum commodo felis quis tortor.</li>
-      <li>Ut aliquam sollicitudin leo.</li>
-      <li>Cras iaculis ultricies nulla.</li>
-      <li>Donec quis dui at dolor tempor interdum.</li>
-      <li>Vivamus molestie gravida turpis.</li>
-      <li>Fusce lobortis lorem at ipsum semper sagittis.</li>
-      <li>Nam convallis pellentesque nisl.</li>
-      <li>Integer malesuada commodo nulla.</li>
-      </ul>
-</aside>
