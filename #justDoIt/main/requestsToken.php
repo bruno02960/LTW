a44efad412dfa9a65ab1fc4f15e3ef243516807b
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2017-12-12 20:48:34 +0000

    added untraked files

---
+++ #justDoIt/main/requestsToken.php
@@ -0,0 +1,15 @@
+<?php
+
+    include('../includes/session.php');
+    //generates a paired key for the specified form,using shar 256 as the encryption algorithm
+    //the list name as the message to encrypt and the User Authentication Token as the key
+
+    $listName = strip_tags($_POST['listName']);
+    if(isset($_SESSION['UserAuthToken'])){
+        echo hash_hmac('sha256', $listName, $_SESSION['UserAuthToken']);
+    }else{
+        echo -1;
+    }
+    return;
+
+?>
